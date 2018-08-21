a4d72107b9a1e093982e2c0d25bb54262de65782
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-08-21 15:13:31 -0300

    Exercício 3.6: passo 2.

--- src/main/java/cotuba/Main.java
+++ src/main/java/cotuba/Main.java
@@ -38,59 +38,7 @@ public class Main {
 				geradorPDF.gera(diretorioDosMD,	arquivoDeSaida);
 
 			} else if ("epub".equals(formato)) {
-				Book epub = new Book();
 
-				PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:**/*.md");
-				Stream<Path> arquivosMD = Stream.empty();
-				try {
-					arquivosMD = Files.list(diretorioDosMD).filter(arquivo -> matcher.matches(arquivo)).sorted();
-				} catch (IOException ex) {
-					throw new RuntimeException(
-							"Erro tentando encontrar arquivos .md em " + diretorioDosMD.toAbsolutePath(), ex);
-				}
-
-				arquivosMD.forEach(arquivoMD -> {
-					Parser parser = Parser.builder().build();
-					Node document = null;
-					try {
-						document = parser.parseReader(Files.newBufferedReader(arquivoMD));
-						document.accept(new AbstractVisitor() {
-							public void visit(Heading heading) {
-								if (heading.getLevel() == 1) {
-									// capítulo
-									String tituloDoCapitulo = ((Text) heading.getFirstChild()).getLiteral();
-									// TODO: usar título do capítulo
-								} else if (heading.getLevel() == 2) {
-									// seção
-								} else if (heading.getLevel() == 3) {
-									// título
-								}
-							}
-
-						});
-					} catch (Exception ex) {
-						throw new RuntimeException("Error parsing file " + arquivoMD, ex);
-					}
-
-					try {
-						HtmlRenderer renderer = HtmlRenderer.builder().build();
-						String html = renderer.render(document);
-
-						// TODO: usar título do capítulo
-						epub.addSection("Capítulo", new Resource(html.getBytes(), MediatypeService.XHTML));
-
-					} catch (Exception ex) {
-						throw new RuntimeException("Erro ao renderizar para HTML o arquivo " + arquivoMD, ex);
-					}
-				});
-
-				EpubWriter epubWriter = new EpubWriter();
-
-				try {
-					epubWriter.write(epub, Files.newOutputStream(arquivoDeSaida));
-				} catch (IOException ex) {
-					throw new RuntimeException("Erro ao criar arquivo EPUB: " + arquivoDeSaida.toAbsolutePath(), ex);
-				}
 			} else {
 				throw new RuntimeException("Formato do ebook inválido: " + formato);
 			}
