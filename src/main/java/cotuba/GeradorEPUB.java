fb8ff17733a7729901d0267e10af9194fffc8e50
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-08-21 16:10:50 -0300

    Exercício 3.14: passo 5.

--- src/main/java/cotuba/GeradorEPUB.java
+++ src/main/java/cotuba/GeradorEPUB.java
@@ -2,6 +2,7 @@ package cotuba;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 
 import nl.siegmann.epublib.domain.Book;
 import nl.siegmann.epublib.domain.Resource;
@@ -12,11 +13,20 @@ public class GeradorEPUB {
 
 	public void gera(Ebook ebook) {
 
+		Path arquivoDeSaida = ebook.getArquivoDeSaida();
+		
 		Book epub = new Book();
 
-		// TODO: usar título do capítulo
-		epub.addSection("Capítulo", new Resource(html.getBytes(), MediatypeService.XHTML));
+		for (Capitulo capitulo : ebook.getCapitulos()) {
 
+	        String html = capitulo.getConteudoHTML();
+
+	        String tituloDoCapitulo = capitulo.getTitulo();
+
+	        epub.addSection(tituloDoCapitulo, new Resource(html.getBytes(), MediatypeService.XHTML));
+
+		}
+		
 		EpubWriter epubWriter = new EpubWriter();
 
 		try {
