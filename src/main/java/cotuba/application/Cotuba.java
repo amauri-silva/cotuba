4ebeaef1ad2932dbf49cdb9931d7d24fe118c6fa
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-08-21 17:49:56 -0300

    Exercício 5.12: passo 2.

--- src/main/java/cotuba/application/Cotuba.java
+++ src/main/java/cotuba/application/Cotuba.java
@@ -22,19 +22,6 @@ public class Cotuba {
 		Ebook ebook = new Ebook(formato, arquivoDeSaida);
 		ebook.adicionaCapitulos(capitulos);
 
-		GeradorEbook gerador;
-		
-    	if ("pdf".equals(formato)) {
-			
-			gerador = new GeradorPDF();
-
-		} else if ("epub".equals(formato)) {
-
-			gerador = new GeradorEPUB();
-		      
-		} else {
-			throw new RuntimeException("Formato do ebook inválido: " + formato);
-		}
     	
     	gerador.gera(ebook);
 
