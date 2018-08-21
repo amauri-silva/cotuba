b7cbe454807c20354b7d4b16e9c3f51290c39791
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-08-21 15:39:51 -0300

    Exercício 3.12: passo 2.

---
+++ src/main/java/cotuba/Ebook.java
@@ -0,0 +1,48 @@
+package cotuba;
+
+  import java.nio.file.Path;
+  import java.util.ArrayList;
+  import java.util.List;
+
+  public class Ebook {
+
+    private String formato;
+
+    private Path arquivoDeSaida;
+
+    private List<Capitulo> capitulos = new ArrayList<>();
+
+    public Ebook(String formato, Path arquivoDeSaida) {
+      this.formato = formato;
+      this.arquivoDeSaida = arquivoDeSaida;
+    }
+
+    public String getFormato() {
+		return formato;
+	}
+
+	public void setFormato(String formato) {
+		this.formato = formato;
+	}
+
+	public Path getArquivoDeSaida() {
+		return arquivoDeSaida;
+	}
+
+	public void setArquivoDeSaida(Path arquivoDeSaida) {
+		this.arquivoDeSaida = arquivoDeSaida;
+	}
+
+	public List<Capitulo> getCapitulos() {
+		return capitulos;
+	}
+
+	public void setCapitulos(List<Capitulo> capitulos) {
+		this.capitulos = capitulos;
+	}
+
+	public void adicionaCapitulos(List<Capitulo> capitulos) {
+      this.capitulos.addAll(capitulos);
+    }
+
+  }
